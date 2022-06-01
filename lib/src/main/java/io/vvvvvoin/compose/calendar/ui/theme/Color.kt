5c0494d8bcc9fadfe24bb1768d48e234662730ac
vvvvvoin <aszx7009@gmail.com>
2022-06-01 16:09:33 +0900

    Move class to another module

---
+++ lib/src/main/java/io/vvvvvoin/compose/calendar/ui/theme/Color.kt
@@ -0,0 +1,78 @@
+package io.vvvvvoin.compose.calendar.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// TODO : make to support dark theme, prepare palette color
+
+// text
+internal val text_dark = Color(0x80000000)
+internal val text_darker = Color(0xB3000000)
+internal val text_darkest = Color(0xFF000000)
+
+
+// icon
+internal val icon_dark = Color(0x4D000000)
+internal val icon_darker = Color(0x99000000)
+internal val icon_darkest = Color(0xE6000000)
+
+
+// background
+internal val bg_white = Color(0xFFFFFFFF)
+
+internal val bg_black = Color(0xCC000000)
+
+internal val bg_transparent = Color(0x00000000)
+
+
+// border
+internal val border_dark = Color(0x26000000)
+
+
+// chromatic color (text, icon, bg)
+internal val bgt_red_normal = Color(0xFFF73030)
+internal val bgt_red_dark = Color(0xB3F73030)
+internal val bgt_red_darker = Color(0x80F73030)
+internal val bgt_red_darkest = Color(0x4DF73030)
+
+internal val bgt_orange_normal = Color(0xFFF78330)
+internal val bgt_orange_dark = Color(0xB3F78330)
+internal val bgt_orange_darker = Color(0x80F78330)
+internal val bgt_orange_darkest = Color(0x4DF78330)
+
+internal val bgt_yellow_normal = Color(0xFFF7E030)
+internal val bgt_yellow_dark = Color(0xB3F7E030)
+internal val bgt_yellow_darker = Color(0x80F7E030)
+internal val bgt_yellow_darkest = Color(0x4DF7E030)
+
+internal val bgt_green_normal = Color(0xFF8AF730)
+internal val bgt_green_dark = Color(0xB38AF730)
+internal val bgt_green_darker = Color(0x808AF730)
+internal val bgt_green_darkest = Color(0x4D8AF730)
+
+internal val bgt_aqua_normal = Color(0xFF30F7D9)
+internal val bgt_aqua_dark = Color(0xB330F7D9)
+internal val bgt_aqua_darker = Color(0x8030F7D9)
+internal val bgt_aqua_darkest = Color(0x4D30F7D9)
+
+internal val bgt_sky_normal = Color(0xFF30A7F7)
+internal val bgt_sky_dark = Color(0xB330A7F7)
+internal val bgt_sky_darker = Color(0x8030A7F7)
+internal val bgt_sky_darkest = Color(0x4D30A7F7)
+
+internal val bgt_blue_normal = Color(0xFF3D30F7)
+internal val bgt_blue_dark = Color(0xB33D30F7)
+internal val bgt_blue_darker = Color(0x803D30F7)
+internal val bgt_blue_darkest = Color(0x4D3D30F7)
+
+internal val bgt_purple_normal = Color(0xFF8330F7)
+internal val bgt_purple_dark = Color(0xB38330F7)
+internal val bgt_purple_darker = Color(0x808330F7)
+internal val bgt_purple_darkest = Color(0x4D8330F7)
+
+internal val bgt_pink_normal = Color(0xFFF730CF)
+internal val bgt_pink_dark = Color(0xB3F730CF)
+internal val bgt_pink_darker = Color(0x80F730CF)
+internal val bgt_pink_darkest = Color(0x4DF730CF)
+
+
+// TODO : extend chromatic color (coral, mint, ..., etc)
