15f7107bdb9614c3b54d89ff4a33aac14693f0d0
vvvvvoin <aszx7009@gmail.com>
2022-06-01 15:54:59 +0900

    Fix main activity

--- app/src/main/java/io/vvvvvoin/compose/calendar/MainActivity.kt
+++ app/src/main/java/io/vvvvvoin/compose/calendar/MainActivity.kt
@@ -4,33 +4,28 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import io.vvvvvoin.compose.calendar.ui.component.CalendarFrame
-import io.vvvvvoin.compose.calendar.ui.theme.CanlendarTheme
+import io.vvvvvoin.compose.calendar.ui.component.Calendar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CanlendarTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    CalendarFrame(
-                            initYear = 2022,
-                            initMonth = 5,
-                            initDay = 7,
-                            onClickConfirm = {
-                                Log.e("compose Test", "value = ${it}")
-                            }
-                    ) {
+            val scrollState = rememberScrollState()
 
-                    }
-                }
+            Column(modifier = Modifier.verticalScroll(scrollState)) {
+                Calendar(
+                        initYear = 2022,
+                        initMonth = 5,
+                        initDay = 7,
+                        onClickConfirm = {
+                            Log.e("compose Test", "value = ${it}")
+                        },
+                        onClickClear = {},
+                )
             }
         }
     }
