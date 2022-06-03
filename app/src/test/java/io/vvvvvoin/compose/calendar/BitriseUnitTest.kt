2c142f9bd883d67521e019033b66f0217b8cab1f
vvvvvoin <aszx7009@gmail.com>
2022-06-03 16:59:27 +0900

    Add bitrise unit test

---
+++ app/src/test/java/io/vvvvvoin/compose/calendar/BitriseUnitTest.kt
@@ -0,0 +1,11 @@
+package io.vvvvvoin.compose.calendar
+
+import org.junit.Assert
+import org.junit.Test
+
+class BitriseUnitTest {
+    @Test
+    fun `비트라이즈 유닛 테스트`() {
+        Assert.assertEquals(4, 2 + 2)
+    }
+}
